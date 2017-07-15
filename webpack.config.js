d5705ce686cd9bb28be5fdd97f2197a050f2030c
Wei <weili.main@gmail.com>
2017-07-15 03:28:28 -0400

    setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
+const path = require('path');
+
 module.exports = {
     entry: './game/index.js',
     output: {
+        path: path.resolve(__dirname, 'dist'),
         filename: 'dist.js'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        port: 9000
     },
     module: {
         loaders: [
             { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
         ]
+    },
+    externals: {
+        'Phaser': 'Phaser'
     }
 }
